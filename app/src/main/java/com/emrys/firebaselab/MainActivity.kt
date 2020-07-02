bffec9f6fcfd1fde72c192a641308811e585e795
Author Name <myolwinoo919@gmail.com>
2020-07-02 21:32:15 +0630

    fcm

---
+++ app/src/main/java/com/emrys/firebaselab/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.emrys.firebaselab
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val TAG = "HahaLOL"
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    Log.d(TAG, "Firebase Token: $token")
+                    Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                })
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/emrys/firebaselab/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.emrys.firebaselab
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        // token ya p heyyyyyy keep htarrrrr
+    }
+}
