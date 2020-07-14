b36644e93ba60d731f34cbbb1accbe49dd0eeef5
Myo Lwin Oo <myolwinoo919@gmail.com>
2020-07-14 10:17:49 +0630

    email, phone, google

--- app/src/main/java/com/emrys/firebaselab/FirstFragment.kt
+++ app/src/main/java/com/emrys/firebaselab/FirstFragment.kt
@@ -1,5 +1,7 @@
 package com.emrys.firebaselab
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
@@ -11,6 +13,9 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.emrys.firebaselab.databinding.FragmentFirstBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -38,38 +43,64 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val storage = Firebase.storage
-        var storageRef = storage.reference
-
-        val androidRef = storageRef.child("android.jpg")
-
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-
-            // Get the data from an ImageView as bytes
-//            binding.imageView.isDrawingCacheEnabled = true
-//            binding.imageView.buildDrawingCache()
-//            val bitmap = (binding.imageView.drawable as BitmapDrawable).bitmap
-//            val baos = ByteArrayOutputStream()
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-//            val data = baos.toByteArray()
-//
-//            var uploadTask = androidRef.putBytes(data)
-//            uploadTask.addOnFailureListener {
-//                // Handle unsuccessful uploads
-//            }
-//                .addOnSuccessListener {
-//                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
-//                // ...
-//                   // here is the fucking url !!
-//                    val url = it.uploadSessionUri
-//            }
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+//            AuthUI.IdpConfig.FacebookBuilder().build(),
+//            AuthUI.IdpConfig.TwitterBuilder().build()
+        )
 
 
-//            val localFile = File.createTempFile("images", "jpg")
+        binding.buttonAuth.setOnClickListener {
+            if (!isLoggedIn()) {
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                    101
+                )
 
-            androidRef.delete()
+            } else {
+                AuthUI.getInstance()
+                    .signOut(requireContext())
+                    .addOnCompleteListener {
+                        refreshAuthButton()
+                    }
 
+            }
+        }
+    }
+
+    private fun isLoggedIn() = FirebaseAuth.getInstance().currentUser != null
+
+    private fun refreshAuthButton() {
+        binding.buttonAuth.text = if (isLoggedIn()) "Logout" else "Login"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        refreshAuthButton()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 101) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(requireContext(), user!!.displayName, Toast.LENGTH_LONG).show()
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
     }
 }
